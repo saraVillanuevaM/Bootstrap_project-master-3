573e021ad92e7764bdc4e0291044918909d3a16b
Your Name <you@example.com>
2021-02-21 08:59:29 +0100

    correccion 1

--- validaciones.js
+++ validaciones.js
@@ -1,6 +1,13 @@
 
 
 
+
+
+
+
+
+
+
 ///validation search///
 
 function myFunction () {
@@ -37,7 +44,7 @@ password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
 
 
-/////////////////Validacion registro//
+////////////////////////////////////////////////////Validacion registro//
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
   'use strict'
@@ -62,36 +69,128 @@ confirm_password.onkeyup = validatePassword;
 
 
 
-/////////////empty field////////
- function validate(){
-
- let datauser = document.getElementById("usernamefield").value;
- let datapass = document.getElementById("password").value;
-let data = document.getElementById("exampleInputEmail1").value;
+/////////////empty field registro form////////
 
 
-if (data==null || data==""){  
-   form1.mail.setCustomValidity("don't let this empty field");  
-  
- } else {
-  form.mail.add('was-validated');
-  }
 
-  
-   if (datapass==null || datapass==""){  
-    form1.pass.setCustomValidity("don't let this empty field");  
-    
-  } else {
-    form1.pass.add('was-validated');
-   }
- 
-    
+const form = document.getElementById('myFormId');
 
-    if (datauser==null || datauser==""){  
-      form1.user.setCustomValidity("don't let this empty fieldh");  
-    
-    } else {
-      form1.user.add('was-validated');
-     }
-   
-    }  
+function registerValidate() {
+	var acumErrores = 0;
+	
+	form.classList.remove('is-invalid');
+	
+	//var inputEmail = document.forms["myForm"]["inputEmail"];
+
+	var inputEmail = document.getElementById('inputEmail');
+	
+	var inputPassword = document.forms["myForm"]["password"];
+	var inputProvince = document.forms["myForm"]["inputProvince"];
+	var inputUser = document.forms["myForm"]["inputUser"];
+	var gridCheck = document.forms["myForm"]["gridCheck"];
+
+	if(inputEmail.value == "") {
+		inputEmail.classList.add("is-invalid");
+		document.getElementById("errorEmail").textContent = "Please fill the field";
+        acumErrores ++;
+    }else if(!validar_email(inputEmail.value)){
+		inputEmail.classList.add("is-invalid");
+		document.getElementById("errorEmail").textContent = "Verifies that the mail is in a valid format";
+		acumErrores ++;
+	}
+
+    if(inputPassword.value == "") {
+	  password.classList.add("is-invalid");
+		document.getElementById("errorpass").textContent = "Please fill the field";
+		acumErrores ++;
+	}
+
+    if(inputProvince.value == "") {
+		inputProvince.classList.add("is-invalid");
+		document.getElementById("errorProvince").textContent = "Please fill the field";
+		acumErrores ++;
+	}
+
+	
+	if(inputUser.value == "" ) {
+		inputUser.classList.add("is-invalid");
+		document.getElementById("errorUser").textContent = "Please fill the field";
+		acumErrores ++;
+	}
+	
+	if(!gridCheck.checked) {
+		gridCheck.classList.add("is-invalid");
+		document.getElementById("errorCheck").textContent = "Check the info";
+		acumErrores ++;
+	}
+
+    if (acumErrores > 0){
+        return false;
+    }else{
+		return true;
+	}
+}
+
+
+
+form.addEventListener('blur', (event) => {
+	console.log(event);
+	if(event.target.value!='') event.target.classList.remove('is-invalid');
+    //registerValidate();
+}, true);
+
+function validar_email(email) {
+	var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	return regex.test(email) ? true : false;
+}
+
+
+
+
+
+
+//////////////////////////////////////sign-it//////////////////////////////////
+
+const form2 = document.getElementById('myFormId2');
+
+function registerValidate2() {
+	var acumErrores2 = 0;
+	
+	
+	
+	//var inputEmail = document.forms["myForm"]["inputEmail"];
+
+	var inputEmail2 = document.getElementById('inputEmail2');
+	
+	var inputPassword2 = document.forms["myForm2"]["inputPassword2"];
+	
+
+	if(inputEmail2.value == "") {
+		inputEmail2.classList.add("is-invalid");
+		document.getElementById("errorEmail2").textContent = "Please fill the field";
+        acumErrores2 ++;
+
+
+    }else if(!validar_email(inputEmail2.value)){
+		inputEmail2.classList.add("is-invalid");
+		document.getElementById("errorEmail2").textContent = "Verifies that the mail is in a valid format";
+		acumErrores2 ++;
+	}
+
+    if(inputPassword2.value == "") {
+		inputPassword2.classList.add("is-invalid");
+		document.getElementById("errorPassword2").textContent = "Please fill the field";
+		acumErrores2 ++;
+	}
+
+
+    if (acumErrores2 > 0){
+        return false;
+    }else{
+		return true;
+	}
+}
+
+
+
+
